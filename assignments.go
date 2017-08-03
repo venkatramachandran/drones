40bae8176cdfbb8f639b19747f0b06b0cf869829
Venkat Ramachandran <rvenkat@gmail.com>
2017-08-03 10:29:47 +1000

    Added an assignments type

---
+++ assignments.go
@@ -0,0 +1,23 @@
+package main
+
+//Assignment is a package to drone assignment
+type Assignment struct {
+	DroneID   int `json:"droneId"`
+	PackageID int `json:"packageId"`
+}
+
+//Output is the returned output
+type Output struct {
+	Assignments       []Assignment `json:"assignments"`
+	UnassignedPakages []int        `json:"unassignedPackageIds"`
+}
+
+//AddAssignment adds a new assignment to the output
+func (o *Output) AddAssignment(droneID, packageID int) {
+	o.Assignments = append(o.Assignments, Assignment{DroneID: droneID, PackageID: packageID})
+}
+
+//AddUnassignedPackage adds a new package to the unassigned list
+func (o *Output) AddUnassignedPackage(packageID int) {
+	o.UnassignedPakages = append(o.UnassignedPakages, packageID)
+}
